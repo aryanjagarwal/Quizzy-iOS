50c560f98d4bcc0539ac10412f752622d9f3c074
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-06-28 08:59:46 +0530

    StoryBoard Set to go

--- Quizzy/ViewController.swift
+++ Quizzy/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qusetionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtonPressesd(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
