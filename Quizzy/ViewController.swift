f8671773284c8471679dd82f9d1a81ecc03ec27f
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-06-28 10:09:00 +0530

    added some questions

--- Quizzy/ViewController.swift
+++ Quizzy/ViewController.swift
@@ -14,15 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = ["Four + two is equal to Six", "Five - Three is greater than one", "Three + Eight is less than ten"]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
     @IBAction func answerButtonPressesd(_ sender: UIButton) {
+        
+        questionNumber += 1
+        updateUI()
     }
     
+    func updateUI() {
+        qusetionLabel.text = quiz[questionNumber]
+        
+    }
     
     
 }
